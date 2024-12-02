7ca69a540baddce38910b8e2940df16a5d6aaf8d
Vlad Miroshin <ppvlad@mail.ru>
2024-12-02 21:29:21 +0300

    add function reverse, add test case

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+import {reverse} from './index.js'
+
+
+test.each([
+    {s: 'abcd', expected: 'dcba'},
+    {s: 'a', expected: 'a'},
+    {s: '1234567890', expected: '0987654321'},
+    {s: 'абвгдеёжз', expected: 'зжёедгвба'},
+  ])('reverse($s, $expected)', ({s, expected}) => {
+    expect(reverse(s)).toBe(expected);
+  });
